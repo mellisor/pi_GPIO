2c52e71806841b725d04b000ba9b9a93443cf12a
mellisor <evanmellisor@gmail.com>
2018-10-26 20:13:12 +0000

    Added a 0 entry for noteGenerator

--- noteGenerator.py
+++ noteGenerator.py
@@ -1,10 +1,11 @@
-import Buzzer
 import json
 
-Notes = dict()
+BaseNotes = { 'C' : 16.35, 'C#' : 17.32, 'Db' : 17.32, 'D' : 18.35, 'D#' : 19.45, 'Eb' : 19.45, 'E' : 20.6, 'F' : 21.83, 'F#' : 23.12, 'Gb' : 23.12, 'G' : 24.5, 'G#' : 25.96, 'Ab' : 25.96, 'A': 27.5, 'A#' : 29.14, 'Bb' : 29.14, 'B' : 30.87 }  
 
-for note in Buzzer.Notes.keys():
-    base = Buzzer.Notes[note]
+Notes = dict()
+Notes[0] = 0
+for note in BaseNotes.keys():
+    base = BaseNotes[note]
     for a in range(8):
         Notes[note + str(a)] = base
         base = base * 2
